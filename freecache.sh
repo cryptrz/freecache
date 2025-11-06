4c75e1935316599fd190278d5087399a0db4af6d
Franck Ridel <68272257+cryptrz@users.noreply.github.com>
2025-11-06 17:56:59 +0100

    Ensure script is executed with root privileges
    Added a check to ensure the script is run as root.

--- freecache.sh
+++ freecache.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# Check root user
+if [[ $(id -u) -ne 0  ]];then
+        echo "Please execute as root"
+        exit 1
+fi
+
+# Display, clear and display again memory usage
 free && sync && echo 3 > /proc/sys/vm/drop_caches && free
